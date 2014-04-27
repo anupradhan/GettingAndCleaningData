ead17d5460830b834a53ca5d538990cbd41b7048
Anu Pradhan <pradhan@gatech.edu>
2014-04-27 03:28:00 -0400

    added merge functionality

--- run_analysis.R
+++ run_analysis.R
@@ -20,11 +20,11 @@ subject_train <- read.table(file="train/subject_train.txt")
 
 ## binding columns y_test and subject_test
 test_temp <- cbind(y_test, subject_test) 
-colnames(test_temp) <- c("Acitivity","Subject")
+colnames(test_temp) <- c("Activity_ID","Subject")
 
 ## binding columns y_train and subject_train
 train_temp <- cbind(y_train, subject_train) 
-colnames(train_temp) <- c("Acitivity","Subject")
+colnames(train_temp) <- c("Activity_ID","Subject")
 
 activity_subject <- rbind(train_temp,test_temp)
 x_variables <- rbind(x_train, x_test)
@@ -35,5 +35,19 @@ rm(list=c("y_test","subject_test","y_train","subject_train", "train_temp","test_
 ## reading features.txt to get feature names and assigning to x_variables
 featurenames <- read.table(file="features.txt", stringsAsFactors=FALSE)
 colnames(x_variables) <- featurenames[,2]
+
+## identifying column names that are associated with mean 
+meanColnames <- featurenames[grep("mean()", featurenames[,2], fixed=TRUE),2]
+
+## identifying column names that are associated with std
+stdColnames <- featurenames[grep("std()", featurenames[,2], fixed=TRUE),2]
+
+## Extracting columns associated with mean and std 
+x_variables <- x_variables[,c(meanColnames, stdColnames)]
+
+## reading activity_labels.txt
+activity_labels <- read.table(file="activity_labels.txt")
+colnames(activity_labels) <- c("Activity_ID", "Activity_Label")
+temp <- merge(activity_subject, activity_labels, by.x="Activity_ID", by.y="Activity_ID")
 
 
